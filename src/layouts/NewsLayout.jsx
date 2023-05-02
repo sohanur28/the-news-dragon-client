b796cbb14471697a3d23b2a0bad398c1c84a4a72
sohanur28 <gsrahman28@gmail.com>
2023-05-02 23:37:58 +0600

    layout

--- src/layouts/NewsLayout.jsx
+++ src/layouts/NewsLayout.jsx
@@ -6,16 +6,13 @@ import { Col, Container, Row } from 'react-bootstrap';
 import LeftNav from '../pages/Shared/LeftNav/LeftNav';
 import RightNav from '../pages/Shared/RightNav/RightNav';
 
-const Main = () => {
+const NewsLayout = () => {
     return (
         <div>
-            <Header></Header>
+           <Header></Header>
             <Container>
                 <Row>
-                    <Col lg={3}>
-                        <LeftNav></LeftNav>
-                    </Col>
-                    <Col lg={6}>
+                    <Col lg={9}>
                         <Outlet></Outlet>
                     </Col>
                     <Col lg={3}>
@@ -23,9 +20,9 @@ const Main = () => {
                     </Col>
                 </Row>
             </Container>
-            <Footer></Footer>
+            <Footer></Footer> 
         </div>
     );
 };
 
-export default Main;
+export default NewsLayout;
