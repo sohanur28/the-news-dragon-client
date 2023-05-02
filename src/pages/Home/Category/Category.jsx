55c06880cf6255a4443aae84a6b5b877ce44f659
sohanur28 <gsrahman28@gmail.com>
2023-05-03 03:44:46 +0600

    setauth

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -8,7 +8,7 @@ const Category = () => {
 
     return (
         <div>
-            <h2>This category News : {categoryNews.length}</h2>
+            { id && <h2>This category News : {categoryNews.length}</h2>}
             {
                 categoryNews.map(news => <NewsCard
                 key={news._id}
